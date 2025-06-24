3212c9a7fff2240ec664506b55338cdabc9a6322
Carl de Guia <carl.2795@gmail.com>
2025-06-25 01:04:56 +0800

    feat(utils): remove temporary logging from getRedirectURL function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,14 +14,6 @@ export function getRedirectURL() {
   url = url.startsWith("http") ? url : `https://${url}`;
   // Make sure to include a trailing `/`.
   url = url.endsWith("/") ? url : `${url}/`;
-  // TODO: Temporary logging for debugging purposes.
-  console.log(
-    "Redirect URL:",
-    url,
-    "///",
-    process?.env?.NEXT_PUBLIC_SITE_URL, // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL, // Automatically set by Vercel.
-    "http://localhost:3000/"
-  );
+
   return url;
 }
