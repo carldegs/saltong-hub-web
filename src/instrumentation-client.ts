05c61a740c0f223c7e3e94e9a7b542eacdfd461a
Carl de Guia <carl.2795@gmail.com>
2026-01-12 23:02:33 +0800

    fix: disable Sentry in non-production environments

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -12,6 +12,8 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  enabled: false,
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
